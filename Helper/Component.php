9d101a8e89cf3fd90255516360c4edcd6d1a5cec
Everett Green <egreen@petsafe.net>
2017-10-31 12:38:47 -0400

    register the module

---
+++ Helper/Component.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * @author     Everett Green
+ * @copyright  2017
+ * @license    Commercial use is forbidden
+ */
+namespace Eg\ChannelLister\Helper;
+class Component extends \Magento\Framework\App\Helper\AbstractHelper
+{
+
+}
